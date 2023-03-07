165babcbf49fe7f83f8aa9fead9b6e652c6a7b11
renu <renusri2908@gmail.com>
2023-03-07 15:14:13 +0530

    final

---
+++ Sweetshop/Migrations/202302281833157_pro.cs
@@ -0,0 +1,16 @@
+namespace Sweetshop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class pro : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
